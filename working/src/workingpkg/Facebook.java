42c9dc4057e8f2693885a4b19bb7ec07a9b5d2dc
Bhaskargitrep <bhaskarcp93@gmail.com>
2023-05-25 23:33:13 -0400

    Initial Project code

---
+++ working/src/workingpkg/Facebook.java
@@ -0,0 +1,23 @@
+package workingpkg;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("WebDriver.Chrome.Driver", "D:\\eclipse\\Webdrivers\\chromedriver_win32");
+
+		WebDriver CD = new ChromeDriver();	
+	
+		CD.findElement(By.xpath(
+		CD.findElement(By.xpath(
+		
+		
+	}
+
+}
